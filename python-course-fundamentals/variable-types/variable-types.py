39084a9a6f2814d327d750aacb6448bc9a8f4c41
lilhareanand22 <lilhareanand22@gmail.com>
2026-08-16 17:22:23 +0530

    added variable and type program

---
+++ python-course-fundamentals/variable-types/variable-types.py
@@ -0,0 +1,10 @@
+
+name : str ="anand"
+age :int = 38
+print(f"My name is {name.upper()} and age is {age}")
+is_android_developer : bool = True
+middle_name : str | None = f"My name is {name.upper()} and age is {age}"
+
+if middle_name is not None:
+    print(len(middle_name))
+    

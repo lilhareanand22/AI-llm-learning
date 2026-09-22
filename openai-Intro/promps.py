0380970646a3e6b70202ba6cb31e16b40a7cccdd
lilhareanand22 <lilhareanand22@gmail.com>
2026-09-22 17:58:01 +0530

    added basic llm prompt

--- openai-Intro/promps.py
+++ openai-Intro/promps.py
@@ -34,5 +34,89 @@ completion = client.chat.completions.create(
     ],
 )
 
-print(completion.choices[0].message.content)
+#print(completion.choices[0].message.content)
+
+# == Chain of thought ===
+completion = client.chat.completions.create(
+    model=model,
+    messages=[
+        {"role": "system", "content": "You are a math tutor."},
+        {
+            "role": "user",
+            "content": "Solve this math problem step by step: If John has 5 apples and gives 2 to Mary, how many does he have left?",
+        },
+    ],
+)
+#print(completion.choices[0].message.content)
+# == Instructional prompts
+completion = client.chat.completions.create(
+    model=model,
+    messages=[
+        {
+            "role": "system",
+            "content": "You a knowledgeable personal trainer and writer.",
+        },
+        {
+            "role": "user",
+            "content": "Write a 300-word summary of the benefits of exercise, using bullet points.",
+        },
+    ],
+)
+#print(completion.choices[0].message.content)
+
+# == Role-playing prompts ===
+completion = client.chat.completions.create(
+    model=model,
+    messages=[
+        {"role": "system", "content": "You are a character in a fantasy novel."},
+        {
+            "role": "user",
+            "content": "Describe the setting of the story.",
+        },
+    ],
+)
+#print(completion.choices[0].message.content)
+
+completion = client.chat.completions.create(
+    model=model,
+    messages=[
+        {"role": "system", "content": "You are a philosopher."},
+        {
+            "role": "user",
+            "content": "What is the meaning of life?",
+        },
+    ],
+)
+#print(completion.choices[0].message.content)
+
+# == temperature and top-p sampling
+completion = client.chat.completions.create(
+    model=model,
+    messages=[
+        {"role": "system", "content": "You are a creative writer."},
+        {"role": "user", "content": "Write a creative tagline for a coffee shop."},
+    ],
+    #temperature=0.9,  # controls the randomness of the output
+    top_p=0.9,  # controls the diversity of the output
+)
+#print(completion.choices[0].message.content)
+# === Combining techniques ===
+completion = client.chat.completions.create(
+    model=model,
+    messages=[
+        {"role": "system", "content": "You are a travel blogger."},
+        {
+            "role": "user",
+            "content": "Write a 500-word blog post about your recent trip to Paris. Make sure to give a step-by-step itinerary of your trip.",
+        },
+    ],
+    temperature=0.9,
+    stream=True,
+    # top_p=0.9,
+)
+# print(completion.choices[0].message.content)
+for chunk in completion:
+    if chunk.choices[0].delta.content is not None:
+        print(chunk.choices[0].delta.content or "", end="")
+
 
